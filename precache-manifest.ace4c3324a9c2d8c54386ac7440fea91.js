6f1366f212349d40f0aa1b0b5e9449962abe1b22
Michael Nava <M1cha3l.nava@gmail.com>
2020-08-01 18:47:17 -0400

    Updates

---
+++ precache-manifest.ace4c3324a9c2d8c54386ac7440fea91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23abf2db3d507f687b527404508aaf5b",
+    "url": "/react-employee-tracker/index.html"
+  },
+  {
+    "revision": "0cdd8298e49366baa59c",
+    "url": "/react-employee-tracker/static/js/2.b406e494.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-tracker/static/js/2.b406e494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a6210f8d274ca618a3",
+    "url": "/react-employee-tracker/static/js/main.91928df1.chunk.js"
+  },
+  {
+    "revision": "c58193bca35bbd54f2cd",
+    "url": "/react-employee-tracker/static/js/runtime-main.0718a10a.js"
+  }
+]);
